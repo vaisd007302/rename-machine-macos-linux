37c01ba68721c8fe469963f81d0760573786aadf
vaisd007302 <79522273+vaisd007302@users.noreply.github.com>
2024-08-01 16:00:02 +0100

    Start Project

---
+++ rename.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+re='^[0-9]*[.][ ]'
+for f in *; do
+  if [[ $f =~ $re ]]; then
+    newf=`echo "$f" | sed -e s/^[0-9]*\.\ //`
+    mv "$f" "$newf"
+    echo "Renaming $f to $newf"
+  fi
+done
